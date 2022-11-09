bba5b25168032b1d48c9d04efcd52b9ad45c0c00
rafeed035 <shahed.rafeed@gmail.com>
2022-11-09 12:19:03 +0600

    #1 add country entity done

---
+++ src/main/java/com/rafeed/wcteaminfodemo/Enity/Country.java
@@ -0,0 +1,32 @@
+package com.rafeed.wcteaminfodemo.Enity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(
+        name = "country_details"
+)
+public class Country {
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.IDENTITY
+    )
+    @Column(
+            name = "country_id"
+    )
+    private int countryId;
+
+    @Column(
+            name = "country_name",
+            nullable = false,
+            unique = true
+    )
+    private String countryName;
+}
