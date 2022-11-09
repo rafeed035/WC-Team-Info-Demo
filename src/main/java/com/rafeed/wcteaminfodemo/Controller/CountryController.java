0f172cf26cdcfbdf64c4180adf337a91c74bb6a5
rafeed035 <shahed.rafeed@gmail.com>
2022-11-09 15:38:41 +0600

    #9 add country controller done

---
+++ src/main/java/com/rafeed/wcteaminfodemo/Controller/CountryController.java
@@ -0,0 +1,52 @@
+package com.rafeed.wcteaminfodemo.Controller;
+
+import com.rafeed.wcteaminfodemo.CustomExceptions.Exceptions.EntityAlreadyExistsException;
+import com.rafeed.wcteaminfodemo.CustomExceptions.Exceptions.EntityNotFoundException;
+import com.rafeed.wcteaminfodemo.Enity.Country;
+import com.rafeed.wcteaminfodemo.Service.CountryService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/country")
+public class CountryController {
+
+    private CountryService countryService;
+
+    public CountryController(CountryService countryService) {
+        this.countryService = countryService;
+    }
+
+    @PostMapping("/save")
+    public Country saveCountry(@RequestBody Country country) throws EntityAlreadyExistsException {
+        return countryService.saveCountry(country);
+    }
+
+    @GetMapping("/getById")
+    public Country getCountryById(@RequestParam int countryId) throws EntityNotFoundException {
+        return countryService.getCountryById(countryId);
+    }
+
+    @GetMapping("/getByName")
+    public Country getCountryByName(@RequestParam String countryName) throws EntityNotFoundException {
+        return countryService.getCountryByName(countryName);
+    }
+
+    @GetMapping("/getAll")
+    public List<Country> getAllCountries(){
+        return countryService.getAllCountries();
+    }
+
+    @PutMapping("/update")
+    public Country updateCountry(@RequestParam int countryId,
+                                 @RequestBody Country country) throws EntityNotFoundException {
+        return countryService.updateCountry(countryId, country);
+    }
+
+    @DeleteMapping("/delete")
+    public String deleteCountry(@RequestParam int countryId) throws EntityNotFoundException {
+        countryService.deleteCountry(countryId);
+        return "Deleted Successfully";
+    }
+}
