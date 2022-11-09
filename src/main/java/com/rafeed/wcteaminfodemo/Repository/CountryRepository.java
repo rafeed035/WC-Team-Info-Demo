d46f20af366ceaeb5b3f8204710392dce0dc10c4
rafeed035 <shahed.rafeed@gmail.com>
2022-11-09 13:26:57 +0600

    #7 add country repository done

---
+++ src/main/java/com/rafeed/wcteaminfodemo/Repository/CountryRepository.java
@@ -0,0 +1,11 @@
+package com.rafeed.wcteaminfodemo.Repository;
+
+import com.rafeed.wcteaminfodemo.Enity.Country;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CountryRepository extends JpaRepository<Country, Integer> {
+    Country getCountryByCountryId(int countryId);
+    Country getCountryByCountryName(int countryName);
+}
