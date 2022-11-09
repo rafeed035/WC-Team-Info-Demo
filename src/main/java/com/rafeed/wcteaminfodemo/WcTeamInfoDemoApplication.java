30b04321aca7b05573aea3306b6dc2cc64fbba55
rafeed035 <shahed.rafeed@gmail.com>
2022-11-09 12:04:13 +0600

    initial commit

---
+++ src/main/java/com/rafeed/wcteaminfodemo/WcTeamInfoDemoApplication.java
@@ -0,0 +1,13 @@
+package com.rafeed.wcteaminfodemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WcTeamInfoDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WcTeamInfoDemoApplication.class, args);
+    }
+
+}
