f0d699a57ae75af0f2b87c303386965f88e2eb5b
rafeed035 <shahed.rafeed@gmail.com>
2022-11-11 20:45:25 +0600

    user entity added

---
+++ src/main/java/com/rafeed/wcteaminfodemo/Enity/User.java
@@ -0,0 +1,11 @@
+package com.rafeed.wcteaminfodemo.Enity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+}
