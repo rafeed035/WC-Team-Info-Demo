2efc2d4260fb029cd632ba18ebc5c03eb9ce8fce
rafeed035 <shahed.rafeed@gmail.com>
2022-11-12 22:25:25 +0600

    #24 add user controller done

---
+++ src/main/java/com/rafeed/wcteaminfodemo/Controller/UserController.java
@@ -0,0 +1,52 @@
+package com.rafeed.wcteaminfodemo.Controller;
+
+import com.rafeed.wcteaminfodemo.CustomExceptions.Exceptions.EntityAlreadyExistsException;
+import com.rafeed.wcteaminfodemo.CustomExceptions.Exceptions.EntityNotFoundException;
+import com.rafeed.wcteaminfodemo.Enity.User;
+import com.rafeed.wcteaminfodemo.Service.UserService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/user")
+public class UserController {
+
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/save")
+    public User saveUser(@RequestBody User user) throws EntityAlreadyExistsException {
+        return userService.saveUser(user);
+    }
+
+    @GetMapping("/getById")
+    public User getUserById(@RequestParam int userId) throws EntityNotFoundException {
+        return userService.getUserById(userId);
+    }
+
+    @GetMapping("/getByEmail")
+    public User getUserByEmail(@RequestParam String email) throws EntityNotFoundException {
+        return userService.getUserByEmail(email);
+    }
+
+    @PutMapping("/update")
+    public User updateUser(@RequestParam int userId,
+                           @RequestBody User user) throws EntityNotFoundException {
+        return userService.updateUser(userId, user);
+    }
+
+    @GetMapping("/getAll")
+    public List<User> getAllUsers(){
+        return userService.getAllUsers();
+    }
+
+    @DeleteMapping("/delete")
+    public String updateUser(@RequestParam int userId) throws EntityNotFoundException {
+        userService.deleteUser(userId);
+        return "Deleted Successfully!";
+    }
+}
