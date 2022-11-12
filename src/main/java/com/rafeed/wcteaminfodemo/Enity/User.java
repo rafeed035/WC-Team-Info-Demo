e8f3675ff8a84bfea48cfc688918034279d8d9b8
rafeed035 <shahed.rafeed@gmail.com>
2022-11-12 21:45:04 +0600

    #20 add user entity done

--- src/main/java/com/rafeed/wcteaminfodemo/Enity/User.java
+++ src/main/java/com/rafeed/wcteaminfodemo/Enity/User.java
@@ -4,8 +4,33 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
+@Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Table(
+        name = "users"
+)
 public class User {
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.IDENTITY
+    )
+    @Column(
+            name = "user_id"
+    )
+    private int userId;
+
+    @Column(
+            nullable = false,
+            unique = true
+    )
+    private String email;
+
+    @Column(
+            nullable = false
+    )
+    private String password;
 }
---
+++ src/main/java/com/rafeed/wcteaminfodemo/Repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.rafeed.wcteaminfodemo.Repository;
+
+import com.rafeed.wcteaminfodemo.Enity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+    User getUserByUserId(int userId);
+    User getUserByEmail(String email);
+}
